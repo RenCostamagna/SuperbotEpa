6978da99f406a66ecc7038850e36a23dc482a7e7
tirri <ren.costamagna@gmail.com>
2024-10-31 09:59:38 -0300

    Configuracion del puerto

--- src/main.py
+++ src/main.py
@@ -11,5 +11,6 @@ app = Flask(__name__)
 app.register_blueprint(message_webhook_bp, url_prefix='/webhook/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', debug=True, port=port)
 
